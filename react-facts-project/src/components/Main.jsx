050a7a33980280812a8d80357220100ff03ccd6a
sahraozdl <seviyyenozdil@gmail.com>
2025-02-28 19:14:18 +0300

    finished project

--- react-facts-project/src/components/Main.jsx
+++ react-facts-project/src/components/Main.jsx
@@ -1,5 +1,16 @@
 export default function Main() {
   return (
-      <h1>Main component here</h1>
+      <>
+      <main className="main">
+      <h1 className="main-h1">Fun facts about React</h1>
+      <ul className="main-ul">
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+      <li>Is maintained by Meta</li>
+      <li>Has well over 200K stars on GitHub</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Was first released in 2013</li>
+      </ul>
+      </main>
+      </>
   )
 }
