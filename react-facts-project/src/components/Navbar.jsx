924bc9a3a5874a55799f3d5ca4a2ed0802612fc5
sahraozdl <seviyyenozdil@gmail.com>
2025-02-28 18:42:37 +0300

    Started ReactFacts Project for code practise

---
+++ react-facts-project/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+      <>
+  <header className="header">
+  <nav className="nav">
+  <img src="react-logo.png" className="nav-logo" alt="react logo" />
+  <text className="nav-logo-text">ReactFacts</text>
+  </nav>
+  </header>    
+      </>
+  )
+}
